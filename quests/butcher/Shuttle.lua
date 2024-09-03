04d44ea74ba1197df03913071897fac6f2c6fdcf
Durham Goode <durham.goode@gmail.com>
2024-09-03 09:54:52 -0700

    Fix Butcherblock boats

--- quests/butcher/Shuttle.lua
+++ quests/butcher/Shuttle.lua
@@ -1,29 +1,33 @@
 function event_spawn(e)
-	eq.set_timer("oot_boat_1min", 20000) -- boat ride every N minutes
+    eq.set_timer("td_boat_start", 10000) -- TD boat offset by 2.5 minutes
 end
 
 function event_timer(e)
-	if(e.timer == "oot_boat_1min") then
+    if (e.timer == "td_boat_start") then
+        eq.stop_timer("td_boat_start")
+        eq.set_timer("td_boat_warning", 20000) -- TD boat every 5 minutes
+    end
+    if(e.timer == "td_boat_warning") then
         -- Only one boat should announce
         if (e.self:GetSpawnPointID() == 326525) then
             local cl = eq.get_entity_list():GetCloseClientList(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 2000);
             for client in cl.entries do
                 if (client.valid) then
-                    client:Message(MT.White, "The boat to Ocean of Tears leaves in one minute! Hurry!");
+                    client:Message(MT.White, "Magnus Boran shouts \"The boat to Timorous Deep leaves in one minute! Run!\"");
                 end
             end
         end
-		eq.stop_timer("oot_boat_1min")
-		eq.set_timer("oot_boat", 10000)
-	end
-	if(e.timer == "oot_boat") then
-		eq.stop_timer("oot_boat")
-		eq.set_timer("oot_boat_1min", 20000)
-		local cl = eq.get_entity_list():GetCloseClientList(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 100);
-		for client in cl.entries do
-			if (client.valid) then 
-				client:MoveZone("oot", -9200, 275, 4);
-			end
-		end
-	end
+        eq.stop_timer("td_boat_warning")
+        eq.set_timer("td_boat_go", 10000)
+    end
+    if(e.timer == "td_boat_go") then
+        eq.stop_timer("td_boat_go")
+        eq.set_timer("td_boat_warning", 20000)
+        local cl = eq.get_entity_list():GetCloseClientList(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 100);
+        for client in cl.entries do
+            if (client.valid) then 
+                client:MoveZone("timorous", -3273, -4564, 20);
+            end
+        end
+    end
 end
---
+++ quests/butcher/Stormbreaker.lua
@@ -0,0 +1,30 @@
+function event_spawn(e)
+    eq.set_timer("oot_boat_start", 1) -- OOT boat offset by 0 minutes
+end
+
+function event_timer(e)
+    if (e.timer == "oot_boat_start") then
+        eq.stop_timer("oot_boat_start")
+        eq.set_timer("oot_boat_warning", 240000) -- OOT boat every 5 minutes
+    end
+    if(e.timer == "oot_boat_warning") then
+        local cl = eq.get_entity_list():GetCloseClientList(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 2000);
+        for client in cl.entries do
+            if (client.valid) then
+                client:Message(MT.White, "Glorin Binfurr shouts \"The boat to Ocean of Tears is getting ready to leave! Hurry!\"");
+            end
+        end
+        eq.stop_timer("oot_boat_warning")
+        eq.set_timer("oot_boat_go", 60000)
+    end
+    if(e.timer == "oot_boat_go") then
+        eq.stop_timer("oot_boat_go")
+        eq.set_timer("oot_boat_warning", 240000)
+        local cl = eq.get_entity_list():GetCloseClientList(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 100);
+        for client in cl.entries do
+            if (client.valid) then 
+                client:MoveZone("oot", -9200, 275, 4);
+            end
+        end
+    end
+end
